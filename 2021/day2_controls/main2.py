78772c75be242a217d5aa28d3182bdc3b5d014a8
mgallimore88 <mgallimore88@gmail.com>
2021-12-02 00:18:25 -0800

    completes day 2 part 2 using object

---
+++ 2021/day2_controls/main2.py
@@ -0,0 +1,40 @@
+# open the file
+with open("input") as file:
+    raw = file.readlines()
+movements = [line.strip("\n") for line in raw]
+
+class Submarine: 
+    def __init__(self):
+        self.horizontal = 0
+        self.depth = 0
+        self.aim = 0
+
+    
+    def update(self, word, number):
+        if word == "forward":
+            self.horizontal += number
+            self.depth += self.aim * number
+        elif word == "up":
+            self.aim -= number
+        elif word == "down":
+            self.aim += number
+
+def word_part(move):
+    word = move.split(" ")[0]
+    return word
+
+
+def number_part(move):
+    number = move.split(" ")[1]
+    return int(number)
+
+
+sub = Submarine()
+
+for move in movements:
+    sub.update(word_part(move),
+               number_part(move))
+
+print(sub.horizontal * sub.depth)
+
+
