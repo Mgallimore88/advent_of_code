6167449fa8d354063e5bd671ce7e046d181b4a21
Michael Gallimore <mike@easy-expense.com>
2022-12-05 19:20:00 -0800

    completes day 4

---
+++ 2022/day_4/solution_1.py
@@ -0,0 +1,28 @@
+# solution to advent of code Dec 4 part 1
+# https://adventofcode.com/2022/day/4
+
+
+file = "input"
+
+running_total = 0
+
+
+def subset(list_1, list_2):
+    if all([item in list_1 for item in list_2]):
+        return True
+
+
+with open(file) as f:
+    lines = (line.rstrip() for line in f)
+    for line in lines:
+        area_a, area_b = line.split(",")
+
+        start_a, end_a = area_a.split("-")
+        area_a = range(int(start_a), int(end_a) + 1)
+
+        start_b, end_b = area_b.split("-")
+        area_b = range(int(start_b), int(end_b) + 1)
+
+        if subset(area_a, area_b) or subset(area_b, area_a):
+            running_total += 1
+print(running_total)
